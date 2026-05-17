fdaa21b151c5df8fb6a1b1a7125012c7fcc2cc50
Tanzim Sheikh <tanzimsheikh68@gmail.com>
2026-05-17 17:38:10 +0530

    adding base URL

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://your-nexachat-backend.example.com/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://instagram-backend-five-iota.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
